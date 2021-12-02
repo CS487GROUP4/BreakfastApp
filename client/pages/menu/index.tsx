06c0014f285bcc36447d69a63b2e609ef4dae5c9
Julaam Diop <jeebe.diop@gmail.com>
2021-12-01 18:34:57 -0600

    Add sample data and dynamically render menu categories from data

--- client/pages/menu/index.tsx
+++ client/pages/menu/index.tsx
@@ -2,20 +2,44 @@ import type { NextPage } from "next";
 import Nav from "../../components/Nav";
 import Footer from "../../components/Footer";
 import MenuSection from "../../components/MenuSection";
+import { useRouter } from "next/router";
+import { GetStaticProps, GetStaticPaths } from "next";
 
-const Menu: NextPage = () => {
+import categoryData from "../../data/categories.json";
+
+interface Data {
+  id: number;
+  category: string;
+  imgUrl: string;
+}
+
+export const getStaticProps: GetStaticProps = (context) => {
+  const data = categoryData.map((category) => category);
+  return {
+    props: {
+      categories: data,
+    },
+  };
+};
+
+const Menu: NextPage<{ categories: Data[] }> = ({ categories }) => {
+  const router = useRouter();
+
   return (
     <div className="bg-def">
       <Nav />
       <main className="py-12 flex flex-col items-center">
         <h1 className="font-bold font-primary text-4xl mb-10"> Menu Items</h1>
         <article className="grid grid-cols-2 gap-8">
-          <MenuSection imgSrc="drinks.png" title="Drinks" />
-          <MenuSection imgSrc="donuts.png" title="Donuts" />
-          <MenuSection imgSrc="bagels.png" title="Bagels" />
-          <MenuSection imgSrc="sandwiches.png" title="Sandwiches" />
-          <MenuSection imgSrc="pancakes.png" title="Pancakes" />
-          <MenuSection imgSrc="burritos.png" title="Burritos" />
+          {categories.length > 0 &&
+            categories.map((category) => {
+              return (
+                <MenuSection
+                  imgSrc={category.imgUrl}
+                  title={category.category}
+                />
+              );
+            })}
         </article>
       </main>
       <Footer />
