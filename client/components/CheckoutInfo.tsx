f894a3a71ab9e88320c7a51b3403c77242a0716d
Julaam Diop <jeebe.diop@gmail.com>
2021-12-01 20:02:58 -0600

    Add checkout info and cart items

---
+++ client/components/CheckoutInfo.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const CheckoutInfo: NextPage = () => {
+  return <div></div>;
+};
+
+export default CheckoutInfo;
--- client/pages/cart.tsx
+++ client/pages/cart.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import Footer from "../components/Footer";
 import Nav from "../components/Nav";
 import Breadcrumb from "../components/Breadcrumb";
+import Link from "next/link";
+import OrderItem from "../components/OrderItem";
+
 const Cart = () => {
   return (
     <div>
@@ -14,7 +17,21 @@ const Cart = () => {
             <h1 className=""> Order Details</h1>
             <a className=""> Edit </a>
           </div>
-          <div className=""></div>
+          <div className="">
+            <OrderItem
+              title="Glazed Donut"
+              imgSrc="donuts.png"
+              options="Single"
+              quantity={3}
+              price={3.0}
+            />
+          </div>
+          <div className="">
+            <h3 className=""> Current Points: 4350</h3>
+            <Link href="/rewards">
+              <a> See Rewards </a>
+            </Link>
+          </div>
         </article>
         <article className=""></article>
       </main>
