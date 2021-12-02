e8cc48fe2ce5e7c79d7af6a333f6d638e9ee67ef
Julaam Diop <jeebe.diop@gmail.com>
2021-12-01 19:12:27 -0600

    Render menu category items dynamically using sample data

--- client/pages/menu/[category].tsx
+++ client/pages/menu/[category].tsx
@@ -1,28 +1,70 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/dist/client/router";
+import { GetStaticPaths, GetStaticProps } from "next";
 import Breadcrumb from "../../components/Breadcrumb";
 import Footer from "../../components/Footer";
 import MenuItem from "../../components/MenuItem";
 import Nav from "../../components/Nav";
 
-const MenuCategory: NextPage = () => {
+import storeData from "../../data/store.json";
+import categoryData from "../../data/categories.json";
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  console.log("params", params);
+  return {
+    props: {
+      // Get only the items where the currentPages URL is == category name
+      items: storeData.filter((item) => {
+        const categoryUrl =
+          item.category.charAt(0).toLowerCase() + item.category.slice(1);
+        return categoryUrl === params?.category;
+      }),
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = () => {
+  const paths = categoryData.map((category) => {
+    const categoryUrl =
+      category.category.charAt(0).toLowerCase() + category.category.slice(1);
+    return {
+      params: {
+        category: categoryUrl.toString(),
+      },
+    };
+  });
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+const MenuCategory: NextPage<{ items: any }> = ({ items }) => {
   const router = useRouter();
   const category: any = router.query.category;
-  const pageTitle = category?.charAt(0)?.toUpperCase() + category?.slice(1);
-
+  const pageTitle = category?.charAt(0).toUpperCase() + category?.slice(1);
+  console.log(items);
   return (
     <div className="bg-def">
-      <main>
+      <main className="">
         <Nav />
         <Breadcrumb current={pageTitle} />
         <h1 className="title">{pageTitle}</h1>
-
-        <section className="grid grid-cols-3 grid-rows-none gap-5 px-20 my-5">
-          <MenuItem title="Chocolate Donut" imgSrc="donuts.png" price={3.0} />
-          <MenuItem title="Strawberry Donut" imgSrc="donuts.png" price={3.0} />
-          <MenuItem title="Apple Fritter" imgSrc="donuts.png" price={3.0} />
-          <MenuItem title="Apple Fritter" imgSrc="donuts.png" price={3.0} />
-        </section>
+        {items.length > 0 ? (
+          <section className="grid grid-cols-3 grid-rows-none gap-5 px-20 my-5">
+            {items.map((item: any) => {
+              return (
+                <MenuItem
+                  title={item.item}
+                  imgSrc={item.imgUrl}
+                  price={item.price}
+                />
+              );
+            })}
+          </section>
+        ) : (
+          <h1 className="text-4xl text-center"> Out of Items</h1>
+        )}
         <Footer />
       </main>
     </div>
--- client/pages/menu/[category]/[item].tsx
+++ client/pages/menu/[category]/[item].tsx
@@ -1,7 +1,37 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { GetStaticPaths, GetStaticProps } from "next";
 
-const Item: NextPage = () => {
-  return <div></div>;
+// export const getStaticProps: GetStaticProps = async (context) => {
+//   return {
+//     props: {},
+//   };
+// };
+
+// export const getStaticPaths: GetStaticPaths = async (context) => {
+//   const paths = storeData.map((items) => {
+//     const nameUrl = items.item.replace(/ /g, "-");
+//     return {
+//       params: {
+//         id: nameUrl,
+//       },
+//     };
+//   });
+//   return {
+//     paths,
+//     fallback: true,
+//   };
+// };
+
+const Item: NextPage = (props) => {
+  const router = useRouter();
+  const id = router.query.id;
+
+  return (
+    <div>
+      <h1>{id}</h1>
+    </div>
+  );
 };
 
 export default Item;
